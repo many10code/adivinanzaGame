fe3edde993bdf90783d33f8fc0bfe588da5daaae
many10code <20213l001064@utcv.edu.mx>
2024-11-15 12:12:50 -0600

    style: aplicar estilos al login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  username: string= ""; 
+  password: string= "";
 
   constructor() { }
 
