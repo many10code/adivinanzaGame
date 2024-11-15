85692b3b2affbb2b83565f49742fee770b166606
many10code <20213l001064@utcv.edu.mx>
2024-11-15 13:27:43 -0600

    :sparkles:feat: agregar funciones

--- src/app/game/game.page.ts
+++ src/app/game/game.page.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamePage implements OnInit {
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onClickAdivinar() {
+  }
+  
+  onClickReiniciar() {
+ 
   }
 
 }
